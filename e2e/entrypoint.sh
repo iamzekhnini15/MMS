ee6144d0f1d13b7b55d69be75180a0162a51e5a5
Ali Zekhnini <ali.zekhnini@student.vinci.be>
2025-07-27 00:27:34 +0200

    fix: Update datasource URL to use localhost and correct wait-for script usage in entrypoint

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Attente des services API et Frontend
-wait-for api-prod:3000 -t 60
-wait-for frontend-prod:5172 -t 60
+./wait-for.sh api-prod:3000 -t 60
+./wait-for.sh frontend-prod:5172 -t 60
 
 # Lancement des tests Playwright
 npx playwright test
