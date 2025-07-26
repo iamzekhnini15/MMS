9fb0ba33255aa197bf71acaaa251b4aab91fc391
Ali Zekhnini <ali.zekhnini@student.vinci.be>
2025-07-27 01:48:21 +0200

    fix: Remove wait-for script and commented out webServer configuration in Playwright config

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -75,10 +75,10 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'npm run dev',
-    cwd: '../frontend',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-  },
+  // webServer: {
+  //  command: 'npm run dev',
+  //  cwd: '../frontend',
+  //  url: 'http://localhost:5173',
+  //  reuseExistingServer: !process.env.CI,
+  // },
 });
