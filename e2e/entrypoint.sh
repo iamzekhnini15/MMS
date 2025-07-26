69a61ca03c502752f22a4b2e98d2007004e1ba88
Ali Zekhnini <ali.zekhnini@student.vinci.be>
2025-07-27 01:04:25 +0200

    fix: Update datasource URL in application.properties, adjust Dockerfile for netcat installation, and modify entrypoint.sh wait-for script timeout

--- e2e/entrypoint.sh
+++ e2e/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Attente des services API et Frontend
-./wait-for.sh api-prod:3000 -t 60
-./wait-for.sh frontend-prod:5172 -t 60
+./wait-for.sh api-prod:3000 60
+./wait-for.sh frontend-prod:5172 60
 
 # Lancement des tests Playwright
 npx playwright test
